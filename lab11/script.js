f48fe8dacb359d6e59a00f64098672b340cb4882
ramazanq <91422175+ramazanq@users.noreply.github.com>
2021-12-01 23:52:18 +0600

    Add files via upload

---
+++ lab11/script.js
@@ -0,0 +1,11 @@
+
+function onlyNumberKey(evt) {
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
+function detailssubmit() {
+    alert("Your details were Submitted");
+}
+
